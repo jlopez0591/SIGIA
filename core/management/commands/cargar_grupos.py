c04d2e5c1af7c6869de51447eabd3768c2348849
Jose Lopez <jose.a.lopez0591@gmail.com>
2018-04-11 10:47:17 -0500

    Agregar permisos y modificar plantillas

--- core/management/commands/cargar_grupos.py
+++ core/management/commands/cargar_grupos.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Group, Permission
 
@@ -7,15 +8,24 @@ logger = logging.getLogger(__name__)
 
 permisos_profesores = ['add_actividad', 'ver_departamento']
 
-permisos_directores_departamento = ['ver_departamento', 'change_actividad']
+permisos_directores_departamento = ['ver_departamento', 'change_actividad', 'ver_perfil', 'aprobar_actividad']
 
-permisos_directores_escuela = ['ver_escuela', 'ver_estudiante', 'ver_anteproyecto']
+# ver departamento = ver actividades del departamento, ver profesores del departamento
+# ver perfil = Poder observar perfiles de otros profesores
 
-permisos_administrativos = ['ver_escuela', 'add_anteproyecto', 'ver_estudiante', 'change_estudiante']
+permisos_directores_escuela = ['ver_escuela', 'ver_estudiante']
 
-permisos_comision = ['ver_escuela', 'aprobar_anteproyecto']
+# Ver escuela = Ver detalle de escuela, ver estudiantes, ver anteproyectos y ver proyectos
+
+permisos_administrativos = ['ver_escuela', 'add_anteproyecto', 'add_trabajo', 'change_estudiante', 'add_aula', 'add_equipo']
+
+# change_estudiante = editar estudiante, tarea de administrativos de la escuela
+
+permisos_comision = ['ver_escuela', 'aprobar_anteproyecto', 'add_trabajo']
 
 permisos_decanos = ['ver_facultad', 'ver_departamento', 'ver_escuela', 'ver_perfil']
+
+# ver facultad = debe poder ver todos los detalles de la facultad.
 
 lista = {
     'Profesores': permisos_profesores,
@@ -35,11 +45,12 @@ class Command(BaseCommand):
             g, created = Group.objects.get_or_create(name=grupo.title())
             if created:
                 logger.info('Grupo {} creado.'.format(grupo))
-                for permiso in permisos:
-                    try:
-                        p = Permission.objects.get(codename=permiso)
+            for permiso in permisos:
+                try:
+                    p = Permission.objects.get(codename=permiso)
+                    if p not in g.permissions.all():
                         g.permissions.add(p)
                         logger.info('Se agrego el permiso {} al grupo {}.'.format(permiso, g))
-                    except:
-                        logger.error('Hubo un error al agregar el permiso {} al grupo {}: {}'.format(permiso, g,
-                                                                                                     sys.exc_info()[1]))
+                except ObjectDoesNotExist:
+                    logger.error('Hubo un error al agregar el permiso {} al grupo {}: {}'.format(permiso, g,
+                                                                                                    sys.exc_info()[1]))
